787cd678f2a7a7a7f61fc1835ded4e3e5137e219
hhyukk <hhyukk1273@gmail.com>
2023-11-13 02:11:32 +0900

    프로젝트 세팅

---
+++ calendar-api/src/main/java/com/larry/project/api/ApiApplication.java
@@ -0,0 +1,40 @@
+package com.larry.project.api;
+
+import com.larry.project.core.SimpleEntity;
+import com.larry.project.core.SimpleEntityRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@EntityScan("com.larry.project.core")
+@EnableJpaRepositories("com.larry.project.core")
+@RestController
+@SpringBootApplication
+public class ApiApplication {
+
+    private final SimpleEntityRepository repository;
+
+    public ApiApplication(SimpleEntityRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<SimpleEntity> findAll(){
+        return repository.findAll();
+    }
+
+    @PostMapping("/save")
+    public SimpleEntity saveOne(){
+        final SimpleEntity e = new SimpleEntity();
+        e.setName("hello");
+        return repository.save(e);
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
